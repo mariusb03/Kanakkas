0f3adb8d184ea6a4d84a5f87fa4adc97d92b25b1
mariusb03 <mariusbr6@gmail.com>
2025-03-05 05:37:31 +0100

    Initial Commit

---
+++ Kanakkas/KanakkasApp.swift
@@ -0,0 +1,32 @@
+//
+//  KanakkasApp.swift
+//  Kanakkas
+//
+//  Created by Marius Bringsvor Rusten on 05/03/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KanakkasApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
